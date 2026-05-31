ffcb7f7816e00675a6ba8ade73ed57520bcb58f0
sam-grouchnikov <sam.grouchnikov@gmail.com>
2026-05-30 22:56:07 -0400

    Valid Anagram

---
+++ src/easy/IsAnagram.kt
@@ -0,0 +1,17 @@
+package easy
+
+class IsAnagram {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) {return false}
+        val arr = IntArray(26)
+        for (i in s.indices) {
+            arr[s[i] - 'a']++
+            arr[t[i] - 'a']--
+        }
+
+        for (check in arr) {
+            if (check != 0 ) return false
+        }
+        return true
+    }
+}
